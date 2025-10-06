9abd8e349d5c6fd07a7937e714616da477c72bf1
Andrew-Joel-Paillacho <168014946+Andrew-Joel-Paillacho@users.noreply.github.com>
2025-10-05 19:04:10 -0500

    Add files via upload

---
+++ datosGif.js
@@ -0,0 +1,17 @@
+const obtenerGifs = async () => {
+
+    const gifContainers = document.getElementById('gifs');
+    const gifTemplate = document.getElementById('gif'); 
+
+    const request = await fetch("https://api.giphy.com/v1/gifs/search?api_key=lerx9FDWD5PH78M207Cj95ac4KTt8G3v&q=goku"); 
+
+    const respuesta = await request.json();
+
+    const gifUrl = respuesta.data[0].images.original.url;
+
+    const img = document.querySelector("#gif img");
+    
+    img.src = gifUrl;
+};
+
+obtenerGifs();
